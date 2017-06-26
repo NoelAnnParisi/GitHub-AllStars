206430803af80c40cc9996e4057d6138d7b200c1
Noel Parisi <ycleptnoel@gmail.com>
2017-06-26 15:52:40 -0700

    modularization

---
+++ app/components/SelectedLanguage.js
@@ -0,0 +1,31 @@
+// move to screens folder & change import on Popular.js
+
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+const SelectedLanguage = props => {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+  return (
+    <ul className="languages">
+      {languages.map(lang => {
+        return (
+          <li
+            key={lang}
+            style={
+              lang === props.selectedLanguage ? {color: 'palevioletred'} : null
+            }
+            onClick={props.onSelect.bind(null, lang)}>
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectedLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
+
+export default SelectedLanguage
