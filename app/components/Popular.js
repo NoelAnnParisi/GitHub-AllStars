dce0d0566961c524c7d8f965b7052f2ecae3754f
Noel Parisi <ycleptnoel@gmail.com>
2017-06-26 21:35:54 -0700

    popular repos to UI

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react'
 import SelectedLanguage from './SelectedLanguage'
+import api from '../utils/app'
+
+import RepoGrid from '../screens/RepoGrid'
 
 export default class Popular extends Component {
   constructor(props) {
     super(props)
     this.state = {
       selectedLanguage: 'All',
+      repos: null,
     }
     this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  componentDidMount() {
+    // fetch repos from Github's API
+    this.updateLanguage(this.state.selectedLanguage)
   }
   updateLanguage(lang) {
     this.setState({
       selectedLanguage: lang,
+      repos: null,
+    })
+    // when a user clicks on a language -> fetch most popular Repo's
+    api.fetchPopularRepos(lang).then(repos => {
+      // update UI w/repos
+      this.setState({
+        repos: repos,
+      })
     })
   }
+
   render() {
     return (
       <div>
@@ -21,6 +38,9 @@ export default class Popular extends Component {
           onSelect={this.updateLanguage}
           selectedLanguage={this.state.selectedLanguage}
         />
+        {!this.state.repos
+          ? <p className="loading">LOADING</p>
+          : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
